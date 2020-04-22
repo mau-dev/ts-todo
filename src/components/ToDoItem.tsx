c2d2d481292c17ac5e53f6abad55efe2e32715dd
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-22 23:25:23 +0800

    initializing a single toDo item

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function ToDoItem() {
-	return (
-		<div className='todo-item'>
-			<h1>single to do item </h1>
-		</div>
-	);
+//@to-do next: 1: add onClick to the remove button to handle the remove toDo functionality
+//@to-do next: 2: make edit mode and view mode for single doTo item, by default item is set to view mode,
+//item on click (or dbl click) should activate the edit mode and display input text form to edit the existing item
+
+interface ToDoItemProps {
+	todo: {
+		content: string;
+		completed: boolean;
+	};
 }
+
+const ToDoItem: React.FC<ToDoItemProps> = ({todo}) => {
+	return (
+		<li>
+			<div className='todo-item-row'>
+				<input type='checkbox' checked={todo.completed} />
+				{todo.content}
+				<button className='remove-todo-btn'>x</button>
+			</div>
+		</li>
+	);
+};
 
 export default ToDoItem;
