2121f068c7dca466b533a4ad8abe4595f28c734c
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-22 23:31:44 +0800

    render some hardcoded todos from the initial array

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Todos from './components/Todos';
 import AddToDo from './components/AddToDo';
+import ToDoItem from './components/ToDoItem';
 
 import './App.css';
 
-function App() {
+type Todo = {
+	content: string;
+	completed: boolean;
+};
+
+const todos: Array<Todo> = [
+	{content: 'finish the book', completed: false},
+	{content: 'exercise', completed: false},
+	{content: 'shop for groceries', completed: true},
+	{content: 'buy the mongoDB course on udemy before promotion ends', completed: true},
+	{content: 'learn TypeScript', completed: false},
+	{content: 'make toDo app', completed: false},
+	{content: 'make recipe calculator', completed: false}
+];
+
+const App: React.FC = () => {
 	return (
 		<div className='App'>
-			<h1>app main </h1>
 			<AddToDo />
-			<Todos />
+			<ToDoItem todo={todos[0]} />
+			<ToDoItem todo={todos[3]} />
+			{/* 	<Todos /> */}
 		</div>
 	);
-}
+};
 
 export default App;
