4af73fced9e879098698b4058e7dbeab4cc6d08d
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-22 23:29:53 +0800

    add toDo form

--- src/components/AddToDo.tsx
+++ src/components/AddToDo.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState, ChangeEvent} from 'react';
 
-function AddToDo() {
+const AddToDo: React.FC = () => {
+	const [ newToDoItem, setNewToDoItem ] = useState<string>('');
+
+	const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setNewToDoItem(e.target.value);
+	};
+
+	const onSubmit = (e: ChangeEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+	};
+
 	return (
-		<div className='add-todo'>
-			<h1>add todo input form and button</h1>
-		</div>
+		<form className='add-todo'>
+			<input
+				type='text'
+				name='content'
+				value={newToDoItem}
+				placeholder='Ex. Finish the book.. '
+				onChange={onChange}
+			/>
+			<button type='submit' onSubmit={onSubmit}>
+				Add To Do{' '}
+			</button>
+		</form>
 	);
-}
+};
 
 export default AddToDo;
